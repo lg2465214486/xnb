d3bf45f2f1f0286ee2ef4cfe7b9489d3ab72e828
rocky <miataxiang@163.com>
2024-03-06 16:56:35 +0800

    k line method (not complete)

--- src/main/java/com/example/xnb/service/AlgorithmService.java
+++ src/main/java/com/example/xnb/service/AlgorithmService.java
@@ -8,5 +8,5 @@ import java.util.List;
 public interface AlgorithmService {
     void addCoinGenerateData(Coin param);
 
-    List<LocalDateTime> generateTimeList(LocalDateTime issueTime);
+    List<LocalDateTime> generateTimeList(LocalDateTime startTime, LocalDateTime endTime);
 }
--- src/main/java/com/example/xnb/service/impl/AlgorithmServiceImpl.java
+++ src/main/java/com/example/xnb/service/impl/AlgorithmServiceImpl.java
@@ -25,46 +25,125 @@ public class AlgorithmServiceImpl implements AlgorithmService {
     @Autowired
     private ICandlestickChartService candlestickChartService;
 
+    private static final Integer DAY_OF_INCREASE = 5;
+    private static final Integer INCREASE = 20;
+    private static final Random MY_RANDOM = new Random();
+
     @Override
     public void addCoinGenerateData(Coin coin) {
+        LocalDateTime now = LocalDateTime.now();
+        now = this.convertThirtyMinute(now);
+        LocalDateTime startTime = LocalDateTime.now().minusMonths(3);
+        startTime = this.convertThirtyMinute(startTime);
+        LocalDateTime midTime = LocalDateTime.now().minusMonths(1);
+        midTime = this.convertThirtyMinute(midTime);
+
+        int max = Math.max(coin.getStartPrice().multiply(new BigDecimal(100)).intValue(), coin.getPrice().multiply(new BigDecimal(100)).intValue()) * (INCREASE + 100) / 100;
+        int min = Math.min(coin.getStartPrice().multiply(new BigDecimal(100)).intValue(), coin.getPrice().multiply(new BigDecimal(100)).intValue()) * (100 - INCREASE) / 100;
+
         List<CandlestickChart> candlestickChartList = new ArrayList<>();
-        List<LocalDateTime> timeList = this.generateTimeList(LocalDateTime.parse("2024-01-01T00:00:00"));
-        Random random = new Random();
-        int max = Math.max(coin.getStartPrice().multiply(new BigDecimal(100)).intValue(), coin.getPrice().multiply(new BigDecimal(100)).intValue()) * 120 / 100;
-        int min = Math.min(coin.getStartPrice().multiply(new BigDecimal(100)).intValue(), coin.getPrice().multiply(new BigDecimal(100)).intValue()) * 80 / 100;
-        for (LocalDateTime time : timeList) {
+        List<LocalDateTime> timeList = this.generateTimeList(startTime, midTime);
+        List<LocalDateTime> lastTimeList = this.generateTimeList(midTime, now);
+
+        Integer startPriceOf24Hours = coin.getStartPrice().multiply(new BigDecimal(100)).intValue();
+        List<Integer> datas = null;
+        int count=48;
+        for (int i = 0; i < timeList.size(); i++) {
+            if (count == 48) {
+                int flag = MY_RANDOM.nextInt(3);
+                if (startPriceOf24Hours * (DAY_OF_INCREASE + 100) / 100 > max) {
+                    flag = 2;
+                } else if (startPriceOf24Hours * (100 - DAY_OF_INCREASE) / 100 < min) {
+                    flag = 1;
+                }
+                datas = this.generate24HoursData(startPriceOf24Hours, flag);
+                startPriceOf24Hours = datas.get(47);
+                count=0;
+            }
             CandlestickChart candlestickChart = new CandlestickChart();
-            candlestickChart.setTime(time);
+            candlestickChart.setTime(timeList.get(i));
             candlestickChart.setCoinId(coin.getId());
-            int price = min + random.nextInt(max - min + 1);
+            int price = datas.get(i % 48);
             candlestickChart.setPrice(new BigDecimal(price).divide(new BigDecimal(100), 2, RoundingMode.HALF_UP));
 
             int thisTimeMax = price * 110 / 100;
             int thisTimeMin = price * 90 / 100;
 
-            candlestickChart.setMaxPrice(new BigDecimal(price + random.nextInt(thisTimeMax - price + 1)).divide(new BigDecimal(100), 2, RoundingMode.HALF_UP));
-            candlestickChart.setMinPrice(new BigDecimal(thisTimeMin + random.nextInt(price - thisTimeMin + 1)).divide(new BigDecimal(100), 2, RoundingMode.HALF_UP));
+            candlestickChart.setMaxPrice(new BigDecimal(price + MY_RANDOM.nextInt(thisTimeMax - price + 1)).divide(new BigDecimal(100), 2, RoundingMode.HALF_UP));
+            candlestickChart.setMinPrice(new BigDecimal(thisTimeMin + MY_RANDOM.nextInt(price - thisTimeMin + 1)).divide(new BigDecimal(100), 2, RoundingMode.HALF_UP));
+            candlestickChartList.add(candlestickChart);
+            count++;
+        }
+        //todo: a
+        Integer p = startPriceOf24Hours;
+        for (int i = 1; i < lastTimeList.size(); i++) {
+            CandlestickChart candlestickChart = new CandlestickChart();
+            candlestickChart.setTime(timeList.get(i));
+            candlestickChart.setCoinId(coin.getId());
+            int price = datas.get(i % 48);
+            candlestickChart.setPrice(new BigDecimal(price).divide(new BigDecimal(100), 2, RoundingMode.HALF_UP));
+
+            int thisTimeMax = price * 110 / 100;
+            int thisTimeMin = price * 90 / 100;
+
+            candlestickChart.setMaxPrice(new BigDecimal(price + MY_RANDOM.nextInt(thisTimeMax - price + 1)).divide(new BigDecimal(100), 2, RoundingMode.HALF_UP));
+            candlestickChart.setMinPrice(new BigDecimal(thisTimeMin + MY_RANDOM.nextInt(price - thisTimeMin + 1)).divide(new BigDecimal(100), 2, RoundingMode.HALF_UP));
             candlestickChartList.add(candlestickChart);
         }
-        candlestickChartService.saveBatch(candlestickChartList);
+
+//        int max = Math.max(coin.getStartPrice().multiply(new BigDecimal(100)).intValue(), coin.getPrice().multiply(new BigDecimal(100)).intValue()) * 120 / 100;
+//        int min = Math.min(coin.getStartPrice().multiply(new BigDecimal(100)).intValue(), coin.getPrice().multiply(new BigDecimal(100)).intValue()) * 80 / 100;
+//        for (LocalDateTime time : timeList) {
+//            CandlestickChart candlestickChart = new CandlestickChart();
+//            candlestickChart.setTime(time);
+//            candlestickChart.setCoinId(coin.getId());
+//            int price = min + random.nextInt(max - min + 1);
+//            candlestickChart.setPrice(new BigDecimal(price).divide(new BigDecimal(100), 2, RoundingMode.HALF_UP));
+//
+//            int thisTimeMax = price * 110 / 100;
+//            int thisTimeMin = price * 90 / 100;
+//
+//            candlestickChart.setMaxPrice(new BigDecimal(price + random.nextInt(thisTimeMax - price + 1)).divide(new BigDecimal(100), 2, RoundingMode.HALF_UP));
+//            candlestickChart.setMinPrice(new BigDecimal(thisTimeMin + random.nextInt(price - thisTimeMin + 1)).divide(new BigDecimal(100), 2, RoundingMode.HALF_UP));
+//            candlestickChartList.add(candlestickChart);
+//        }
+//        candlestickChartService.saveBatch(candlestickChartList);
     }
 
     @Override
-    public List<LocalDateTime> generateTimeList(LocalDateTime issueTime) {
+    public List<LocalDateTime> generateTimeList(LocalDateTime startTime, LocalDateTime endTime) {
         List<LocalDateTime> list = new ArrayList<>();
-        LocalDateTime now = LocalDateTime.now();
-        now.withSecond(0);
-        if (now.getMinute() > 0 && now.getMinute() < 30) {
-            now.withMinute(30);
-        } else if (now.getMinute() > 30) {
-            now.withMinute(0).plusHours(1);
-        }
-
-        LocalDateTime current = issueTime;
-        while (current.isBefore(now) || current.isEqual(now)) {
+        LocalDateTime current = startTime;
+        while (current.isBefore(endTime) || current.isEqual(endTime)) {
             list.add(current);
             // Increment by half an hour
             current = current.plusMinutes(30);
+        }
+        return list;
+    }
+
+    public LocalDateTime convertThirtyMinute(LocalDateTime time) {
+        time.withSecond(0);
+        if (time.getMinute() > 0 && time.getMinute() < 30) {
+            time.withMinute(30);
+        } else if (time.getMinute() > 30) {
+            time.withMinute(0).plusHours(1);
+        }
+        return time;
+    }
+
+    public List<Integer> generate24HoursData(Integer price, int flag) {
+        List<Integer> list = new ArrayList<>();
+
+        switch (flag) {
+            case 0:
+                break;
+            case 1:
+                break;
+            case 2:
+                break;
+            default:
+                break;
         }
         return list;
     }
