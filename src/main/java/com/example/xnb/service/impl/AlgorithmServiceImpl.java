9e19b497d4538c2960ae5ac81e6dc1a8c632242c
rocky <miataxiang@163.com>
2024-03-09 03:59:19 +0800

    k line

--- src/main/java/com/example/xnb/service/impl/AlgorithmServiceImpl.java
+++ src/main/java/com/example/xnb/service/impl/AlgorithmServiceImpl.java
@@ -35,7 +35,7 @@ public class AlgorithmServiceImpl implements AlgorithmService {
         now = this.convertThirtyMinute(now);
         LocalDateTime startTime = LocalDateTime.now().minusMonths(3);
         startTime = this.convertThirtyMinute(startTime);
-        LocalDateTime midTime = LocalDateTime.now().minusMonths(1);
+        LocalDateTime midTime = LocalDateTime.now().minusDays(1);
         midTime = this.convertThirtyMinute(midTime);
 
         int max = Math.max(coin.getStartPrice().multiply(new BigDecimal(100)).intValue(), coin.getPrice().multiply(new BigDecimal(100)).intValue()) * (INCREASE + 100) / 100;
@@ -51,12 +51,12 @@ public class AlgorithmServiceImpl implements AlgorithmService {
         for (int i = 0; i < timeList.size(); i++) {
             if (count == 48) {
                 int flag = MY_RANDOM.nextInt(3);
-                if (startPriceOf24Hours * (DAY_OF_INCREASE + 100) / 100 > max) {
-                    flag = 2;
-                } else if (startPriceOf24Hours * (100 - DAY_OF_INCREASE) / 100 < min) {
-                    flag = 1;
-                }
-                datas = this.generate24HoursData(startPriceOf24Hours, flag);
+//                if (startPriceOf24Hours * (DAY_OF_INCREASE + 100) / 100 > max) {
+//                    flag = 2;
+//                } else if (startPriceOf24Hours * (100 - DAY_OF_INCREASE) / 100 < min) {
+//                    flag = 1;
+//                }
+                datas = this.generate24HoursData(startPriceOf24Hours, flag, max, min);
                 startPriceOf24Hours = datas.get(47);
                 count=0;
             }
@@ -76,12 +76,13 @@ public class AlgorithmServiceImpl implements AlgorithmService {
             count++;
         }
         Integer lastPrice = coin.getPrice().multiply(new BigDecimal(100)).intValue();
-        Integer coefficient = 0;
-        if (startPriceOf24Hours.compareTo(coin.getPrice().multiply(new BigDecimal(100)).intValue()) > 0) {
-            coefficient = lastPrice / startPriceOf24Hours / lastTimeList.size();
-        } else if (startPriceOf24Hours.compareTo(coin.getPrice().multiply(new BigDecimal(100)).intValue()) < 0) {
-            coefficient = -(startPriceOf24Hours / lastPrice / lastTimeList.size());
-        }
+//        Integer coefficient = 0;
+//        if (startPriceOf24Hours.compareTo(coin.getPrice().multiply(new BigDecimal(100)).intValue()) > 0) {
+//            coefficient = -((startPriceOf24Hours - lastPrice) / lastTimeList.size());
+//        } else if (startPriceOf24Hours.compareTo(coin.getPrice().multiply(new BigDecimal(100)).intValue()) < 0) {
+//            coefficient = (lastPrice - startPriceOf24Hours) / lastTimeList.size();
+//        }
+        List<Integer> sss = this.generateArray(lastPrice - startPriceOf24Hours);
         Integer p = startPriceOf24Hours;
         for (int i = 1; i < lastTimeList.size(); i++) {
             CandlestickChart candlestickChart = new CandlestickChart();
@@ -89,9 +90,13 @@ public class AlgorithmServiceImpl implements AlgorithmService {
             candlestickChart.setWeek(lastTimeList.get(i).getDayOfWeek().getValue());
             candlestickChart.setCoinId(coin.getId());
 
-            int price = this.randomPrice(p, p * (100 + coefficient) / 100);
-            candlestickChart.setPrice(new BigDecimal(price).divide(new BigDecimal(100), 2, RoundingMode.HALF_UP));
-
+            int price = p + sss.get(i - 1);
+//            int price = this.randomPrice(p, p + coefficient);
+            if (i == (lastTimeList.size() - 1)) {
+                candlestickChart.setPrice(coin.getPrice());
+            } else {
+                candlestickChart.setPrice(new BigDecimal(price).divide(new BigDecimal(100), 2, RoundingMode.HALF_UP));
+            }
             int thisTimeMax = price * 110 / 100;
             int thisTimeMin = price * 90 / 100;
 
@@ -100,23 +105,6 @@ public class AlgorithmServiceImpl implements AlgorithmService {
             candlestickChartList.add(candlestickChart);
             p = price;
         }
-
-//        int max = Math.max(coin.getStartPrice().multiply(new BigDecimal(100)).intValue(), coin.getPrice().multiply(new BigDecimal(100)).intValue()) * 120 / 100;
-//        int min = Math.min(coin.getStartPrice().multiply(new BigDecimal(100)).intValue(), coin.getPrice().multiply(new BigDecimal(100)).intValue()) * 80 / 100;
-//        for (LocalDateTime time : timeList) {
-//            CandlestickChart candlestickChart = new CandlestickChart();
-//            candlestickChart.setTime(time);
-//            candlestickChart.setCoinId(coin.getId());
-//            int price = min + random.nextInt(max - min + 1);
-//            candlestickChart.setPrice(new BigDecimal(price).divide(new BigDecimal(100), 2, RoundingMode.HALF_UP));
-//
-//            int thisTimeMax = price * 110 / 100;
-//            int thisTimeMin = price * 90 / 100;
-//
-//            candlestickChart.setMaxPrice(new BigDecimal(price + random.nextInt(thisTimeMax - price + 1)).divide(new BigDecimal(100), 2, RoundingMode.HALF_UP));
-//            candlestickChart.setMinPrice(new BigDecimal(thisTimeMin + random.nextInt(price - thisTimeMin + 1)).divide(new BigDecimal(100), 2, RoundingMode.HALF_UP));
-//            candlestickChartList.add(candlestickChart);
-//        }
         candlestickChartService.saveBatch(candlestickChartList);
     }
 
@@ -133,34 +121,68 @@ public class AlgorithmServiceImpl implements AlgorithmService {
     }
 
     public LocalDateTime convertThirtyMinute(LocalDateTime time) {
-        time.withSecond(0);
+        time = time.withSecond(0);
         if (time.getMinute() > 0 && time.getMinute() < 30) {
-            time.withMinute(30);
+            time = time.withMinute(30);
         } else if (time.getMinute() > 30) {
-            time.withMinute(0).plusHours(1);
+            time = time.withMinute(0).plusHours(1);
         }
         return time;
     }
 
-    public List<Integer> generate24HoursData(Integer price, int flag) {
+    public List<Integer> generate24HoursData(Integer price, int flag, Integer max, Integer min) {
         List<Integer> list = new ArrayList<>();
 
-        switch (flag) {
-            case 0:
-                break;
-            case 1:
-                break;
-            case 2:
-                break;
-            default:
-                break;
+        int ran = 12 + MY_RANDOM.nextInt(23);
+        for (int i = 0; i < 48; i++) {
+            if (i%ran == 0) {
+                flag = MY_RANDOM.nextInt(3);
+            }
+            switch (flag) {
+                case 0:
+                    price = price * 97 / 100 + MY_RANDOM.nextInt(price * 6 / 100 + 1);
+                    list.add(price);
+                    break;
+                case 1:
+                    price = price + MY_RANDOM.nextInt(price * 3 / 100 + 1);
+                    list.add(price);
+                    break;
+                case 2:
+                    price = price * 97 / 100 + MY_RANDOM.nextInt(price * 3 / 100 + 1);
+                    list.add(price);
+                    break;
+                default:
+                    break;
+            }
+            if (price > max) {
+                flag = 2;
+            } else if (price < min) {
+                flag = 1;
+            }
         }
         return list;
     }
 
     public Integer randomPrice(Integer price1, Integer price2) {
-        Integer max = Math.max(price1, price2);
-        Integer min = Math.min(price1, price2);
-        return min + MY_RANDOM.nextInt(max - min + 1);
+        if (price1 < price2) {
+            return price1 + MY_RANDOM.nextInt((price2 - price1) * 2 + 1);
+        } else {
+            return price1 - MY_RANDOM.nextInt((price1 - price2) * 2 + 1);
+        }
+    }
+
+    public List<Integer> generateArray(int sum) {
+        List<Integer> array = new ArrayList<>();
+        int currentSum = 0;
+        Random random = new Random();
+
+        while (currentSum != sum) {
+            int value = random.nextInt(20) - 10;  // 生成-10 到 10 之间的随机数
+            if (currentSum > 47)
+                break;
+            array.add(value);
+            currentSum++;
+        }
+        return array;
     }
 }
