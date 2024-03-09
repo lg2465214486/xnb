9158a8ed2b4db4d5443458169585db4e65f60ae6
rocky <miataxiang@163.com>
2024-03-09 17:24:34 +0800

    real bug fix

--- src/main/java/com/example/xnb/entity/Real.java
+++ src/main/java/com/example/xnb/entity/Real.java
@@ -1,6 +1,7 @@
 package com.example.xnb.entity;
 
 
+import com.baomidou.mybatisplus.annotation.TableName;
 import lombok.Data;
 import lombok.EqualsAndHashCode;
 import lombok.experimental.Accessors;
@@ -16,6 +17,7 @@ import java.time.LocalDateTime;
 @Data
 @EqualsAndHashCode(callSuper = false)
 @Accessors(chain = true)
+@TableName("`real`")
 public class Real {
 
     private static final long serialVersionUID = 1L;
